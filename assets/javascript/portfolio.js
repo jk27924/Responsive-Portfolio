44266175ddb3367b54b846c45053cd5f2fdd6eb8
Jihong Kim <jk279242naver.com>
2019-07-26 21:52:16 -0500

    added all other divs for each of apps, and created portfolio.js to work on show and hide function.

---
+++ assets/javascript/portfolio.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+    $("#psychic-button").on("click", psychic);
+
+    function psychic() {
+
+        $("#psychic").show();
+        $("#crystal").hide();
+    }
+
+
+
+
+
+})
