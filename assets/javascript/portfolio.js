98b61f669236b6373e250851dab70d69c86eaf01
Jihong Kim <jk279242naver.com>
2019-07-27 18:54:45 -0500

    1) added linked in account to footer linked-in icon. 2)fixed portfolio.js so portfolio page only shows one app div per one click event on side bar menu. 3) Added dropdown to the hamburger button that shows when screen size is reduced from nav bar links.

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,15 +1,38 @@
 $(document).ready(function () {
 
-    $("#psychic-button").on("click", psychic);
+// CASE #1
+    // 1) Once enter to Portfolio page, it shows all the portfolio app divs
+    // 2) And then If user wants to see a specific app right away, rather than finding on by scrolling down, the user can click one of side bar menu.
+    // 3) Then only chosen app will show
 
-    function psychic() {
+// CASE #1 - CODE
+    // $(".portfolio-selector").click(function() {
+    //     $(".portfolio-app").hide();
+    //     var id = $(this).attr("data-item");
+    //     $("#" + id).show(); 
+    // });
 
-        $("#psychic").show();
-        $("#crystal").hide();
+
+// -----------------------------------------------------
+
+
+// CASE #2
+    // 1) Once enter to Portfolio page, the first app div, which is psychic game will show, and all other apps will hide.
+    // 2) User can simply use side bar menu to click to see and to find specific app.
+
+// CASE #2 - CODE
+
+    function show(id) {
+        $(".portfolio-app").hide();
+        $("#" + id).show();
     }
+  
+    show("psychic");
+  
+    $(".portfolio-selector").click(function() {
+        
+        var id = $(this).attr("data-item");
+        show(id);
+    });
 
-
-
-
-
-})
+});
