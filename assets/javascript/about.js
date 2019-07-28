bea6ac8d23db925b0d803e8cd2956ffad2cca292
Jihong Kim <jk279242naver.com>
2019-07-27 23:58:47 -0500

    added resume in about page.

---
+++ assets/javascript/about.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+// CASE #1
+    // 1) Once enter to Portfolio page, it shows all the portfolio app divs
+    // 2) And then If user wants to see a specific app right away, rather than finding on by scrolling down, the user can click one of side bar menu.
+    // 3) Then only chosen app will show
+
+// CASE #1 - CODE
+    // $(".bio-selector").click(function() {
+    //     $(".bio-resume").hide();
+    //     var id = $(this).attr("data-item");
+    //     $("#" + id).show(); 
+    // });
+
+
+// -----------------------------------------------------
+
+
+// CASE #2
+    // 1) Once enter to Portfolio page, the first app div, which is psychic game will show, and all other apps will hide.
+    // 2) User can simply use side bar menu to click to see and to find specific app.
+
+// CASE #2 - CODE
+
+    
+  
+
+    // $(".bio-selector").click(function() {
+        
+    //     var id = $(this).attr("data-item");
+    //     show(id);
+    // });
+
+    // function show(id) {
+    //     $(".bio-resume").hide();
+    //     $("#" + id).show();
+    // }
+
+    // show("brief-intro");
+
+
+
+
+});
